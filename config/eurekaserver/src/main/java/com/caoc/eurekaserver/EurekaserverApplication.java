a5f5a46b542e735142ffe90d25adfa7ebaca2cfd
caobandoc <obandocarlosandres@gmail.com>
2023-09-22 11:57:26 -0500

    circuit breaker - no implement

--- config/eurekaserver/src/main/java/com/caoc/eurekaserver/EurekaserverApplication.java
+++ config/eurekaserver/src/main/java/com/caoc/eurekaserver/EurekaserverApplication.java
@@ -1,4 +1,4 @@
-package com.caoc.eurekaservice;
+package com.caoc.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaserviceApplication {
+public class EurekaserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserviceApplication.class, args);
+		SpringApplication.run(EurekaserverApplication.class, args);
 	}
 
 }
--- config/gatewayserver/src/main/java/com/caoc/gatewayserver/GatewayserverApplication.java
+++ config/gatewayserver/src/main/java/com/caoc/gatewayserver/GatewayserverApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayserviceApplication {
+public class GatewayserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayserviceApplication.class, args);
+		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
 }

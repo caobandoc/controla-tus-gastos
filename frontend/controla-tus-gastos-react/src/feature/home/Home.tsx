676386ee36f6579c04d0bcd1195fb72cf0d73264
caobandoc <obandocarlosandres@gmail.com>
2023-12-11 10:17:48 -0500

    update home

--- frontend/controla-tus-gastos-react/src/feature/home/Home.tsx
+++ frontend/controla-tus-gastos-react/src/feature/home/Home.tsx
@@ -2,29 +2,39 @@ import "./home.css";
 
 //mui
 import Button from "@mui/material/Button";
-import { useTheme } from "@mui/material/styles";
 import PersonIcon from "@mui/icons-material/Person";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 //router
 import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
-    const theme = useTheme();
-    const primaryColor = theme.palette.primary.main;
     return (
-        <nav style={{ backgroundColor: primaryColor }}>
-            <span>Controla tus gastos</span>
-            <span className="spacer"></span>
-            <Link to={"/login"}>
-                <Button
-                    variant="outlined"
-                    startIcon={<PersonIcon color="primary" />}
-                    style={{ backgroundColor: "#fff" }}
-                >
-                    Ingresar
-                </Button>
-            </Link>
-        </nav>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        Controla tus gastos
+                    </Typography>
+                    <Link to={"/login"}>
+                        <Button
+                            variant="outlined"
+                            startIcon={<PersonIcon color="primary" />}
+                            style={{ backgroundColor: "#fff" }}
+                        >
+                            Ingresar
+                        </Button>
+                    </Link>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 };
 
